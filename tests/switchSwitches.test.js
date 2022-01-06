dbb0c153f7c4d60247bf4f661dcc6dc6ef2f1174
Martial Anouman <martialanouman@arolitec.com>
2022-01-06 16:45:23 +0000

    add two new challenges

---
+++ tests/switchSwitches.test.js
@@ -0,0 +1,19 @@
+import switchSwitches from "../challenges/swtchSwitches"
+
+describe("switchSwitches", () => {
+  it("should return [1] when calling switchSwitches(1)", () => {
+    expect(switchSwitches(1)).toEqual([1])
+  })
+
+  it("should return [1] when calling switchSwitches(2)", () => {
+    expect(switchSwitches(2)).toEqual([1])
+  })
+
+  it("should return [1,4] when calling switchSwitches(4)", () => {
+    expect(switchSwitches(4)).toEqual([1, 4])
+  })
+
+  it("should return [1,4,9] when calling switchSwitches(9)", () => {
+    expect(switchSwitches(1)).toEqual([1])
+  })
+})
